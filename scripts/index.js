f6b8d67e026de272f5d87495d18304cbcc48ddc3
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-09 08:02:32 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,29 @@
-var slide_index = 1;  
+    var slide_index = 1; 
 
-displaySlides(slide_index);  
-  
-function nextSlide(n) {  
-    displaySlides(slide_index += n);  
-}  
-  
-function currentSlide(n) {  
-    displaySlides(slide_index = n);  
-}  
-  
-function displaySlides(n) {  
-    var i;
-    var slides = document.getElementsByClassName("showSlide");  
-    if (n > slides.length) { slide_index = slides.length }  
-    if (n < 1) { slide_index = 1 }  
-    for (i = 0; i < slides.length; i++) {  
-        slides[i].style.display = "none";  
+    displaySlides(slide_index);  
+    
+    function nextSlide(n) {  
+        displaySlides(slide_index += n);  
     }  
-    slides[slide_index - 1].style.display = "block";  
-}  
+    
+    function currentSlide(n) {  
+        displaySlides(slide_index = n);  
+    }  
+    
+    function displaySlides(n) {  
+        var i;
+        var slides = document.getElementsByClassName("showSlide");  
+        if (n >= slides.length) { slide_index = slides.length }  
+        if (n < 1) { slide_index = 1 }  
+        for (i = 0; i < slides.length; i++) {  
+            slides[i].style.display = "none";  
+        }  
+        slides[slide_index - 1].style.display = "block";  
+    } 
+
+    setInterval(function () {
+        var slides = document.getElementsByClassName("showSlide"); 
+        document.getElementsByTagName("img")[0].src = "assets/avenger" + ++slide_index + ".jpeg";
+        if (slide_index >= slides.length) { slide_index=0 }
+    }, 2000);
+    
